cbcf0d6fef2ddb5c75bf5e14d5b1d4bfd8b8eb5c
Thiago Araujo <thiago-in@hotmail.com>
2019-08-21 23:20:09 -0400

    Parte 1 do agendamento.

--- Site/Controllers/CaixaController.cs
+++ Site/Controllers/CaixaController.cs
@@ -1,4 +1,7 @@
 ﻿using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain.Interfaces;
 using LazZiya.TagHelpers.Alerts;
 using Microsoft.AspNetCore.Mvc;
 using Site.Abstraction;
@@ -8,15 +11,87 @@ namespace Site.Controllers
 {
     public class CaixaController : AbstractController
     {
+        #region Construtor
+
+        private readonly ICliente _cliente;
+        private readonly IAnimal _animal;
+        private readonly IServico _servico;
+        private readonly IUsuario _usuario;
+
+        public CaixaController(ICliente cliente,
+            IAnimal animal,
+            IServico servico,
+            IUsuario usuario)
+        {
+            _cliente = cliente;
+            _animal = animal;
+            _servico = servico;
+            _usuario = usuario;
+        }
+
+        #endregion
+
         public IActionResult Registro()
         {
             return View();
         }
+
+        #region Json Aux
+
+        [HttpGet]
+        public async Task<JsonResult> GetClientes()
+        {
+            var clientes = await _cliente.GetAllAsync(x => x.Ativo == 1);
+            return Json(clientes.Select(x => new
+            {
+                x.Id,
+                nome = x.Nome + " - " + x.Cpf
+            }).OrderBy(x => x.nome));
+        }
+
+        [HttpGet]
+        public async Task<JsonResult> GetPets(int clienteId)
+        {
+            var pets = await _animal.GetAllAsync(x => x.ClienteId == clienteId);
+            return Json(pets.Select(x => new
+            {
+                x.Id,
+                nome = x.Nome
+            }).OrderBy(x => x.nome));
+        }
+
+        [HttpGet]
+        public async Task<JsonResult> GetServicos()
+        {
+            var servicos = await _servico.GetAllAsync(x => x.Ativo == 1);
+            return Json(servicos.Select(x => new
+            {
+                x.Id,
+                nome = x.Nome
+            }).OrderBy(x => x.nome));
+        }
+
+        [HttpGet]
+        public async Task<JsonResult> GetUsuarios()
+        {
+            var usuarios = await _usuario.GetAllAsync(x => x.Ativo == 1);
+            return Json(usuarios.Select(x => new
+            {
+                x.Id,
+                nome = x.Nome
+            }).OrderBy(x => x.nome));
+        }
+
+        #endregion
+
+        #region Outros
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        #endregion
     }
 }
---
+++ Site/wwwroot/paginas/servico-agendamento.js
@@ -0,0 +1,81 @@
+﻿
+function abrirModal() {
+    $("#mdlTitle").text("Novo Agendamento");
+
+    getClientes();
+    getServicos();
+    getUsuarios();
+
+    $("#mdlAgendamento").modal("show");
+}
+
+$("#ddlCliente").change(function (e) {
+    getPets();
+});
+
+function getClientes() {
+
+    $("#ddlCliente > option").remove();
+    var option;
+    $.get("/caixa/GetClientes")
+        .done(function (response) {
+            option = "<option>-- selecione --</option>";
+            $.each(response,
+                function (i, item) {
+                    option += "<option value='" + item.id + "'>" + item.nome + "</option>";
+                });
+
+            $("#ddlCliente").append(option);
+        });
+
+}
+
+function getPets() {
+
+    $("#ddlAnimal > option").remove();
+    var option;
+    $.get("/caixa/GetPets", { clienteId: $("#ddlCliente").val() })
+        .done(function (response) {
+            $.each(response,
+                function (i, item) {
+                    option += "<option value='" + item.id + "'>" + item.nome + "</option>";
+                });
+
+            $("#ddlAnimal").append(option);
+        });
+
+}
+
+function getServicos() {
+
+    $("#ddlServico > option").remove();
+    var option;
+    $.get("/caixa/GetServicos")
+        .done(function (response) {
+            option = "<option>-- selecione --</option>";
+            $.each(response,
+                function (i, item) {
+                    option += "<option value='" + item.id + "'>" + item.nome + "</option>";
+                });
+
+            $("#ddlServico").append(option);
+        });
+
+}
+
+function getUsuarios() {
+
+    $("#ddlUsuario > option").remove();
+    var option;
+    $.get("/caixa/GetUsuarios")
+        .done(function (response) {
+            option = "<option>-- selecione --</option>";
+            $.each(response,
+                function (i, item) {
+                    option += "<option value='" + item.id + "'>" + item.nome + "</option>";
+                });
+
+            $("#ddlUsuario").append(option);
+        });
+
+}
